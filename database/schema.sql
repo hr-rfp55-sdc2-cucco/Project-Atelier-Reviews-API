afff0b19658a6d65f78ee9ce82b1a2de9bd67982
Daniel Ho <dho1994@gmail.com>
2021-10-08 13:20:38 -0700

    Added indexes to postgres

--- database/schema.sql
+++ database/schema.sql
@@ -71,6 +71,31 @@ ALTER TABLE characteristics ADD CONSTRAINT characteristics_product_id_fkey FOREI
 ALTER TABLE characteristic_reviews ADD CONSTRAINT characteristic_reviews_characteristic_id_fkey FOREIGN KEY (characteristic_id) REFERENCES characteristics(id);
 ALTER TABLE characteristic_reviews ADD CONSTRAINT characteristic_reviews_review_id_fkey FOREIGN KEY (review_id) REFERENCES reviews(id);
 
+CREATE INDEX characteristic_reviews_characteristic_id_idx
+    ON public.characteristic_reviews USING btree
+    (characteristic_id ASC NULLS LAST)
+;
+
+CREATE INDEX characteristic_reviews_review_id_idx
+    ON public.characteristic_reviews USING btree
+    (review_id ASC NULLS LAST)
+;
+
+CREATE INDEX characteristics_product_id_idx
+    ON public.characteristics USING btree
+    (product_id ASC NULLS LAST)
+;
+
+CREATE INDEX reviews_product_id_idx
+    ON public.reviews USING btree
+    (product_id ASC NULLS LAST)
+;
+
+CREATE INDEX reviews_photos_review_id_idx
+    ON public.reviews_photos USING btree
+    (review_id ASC NULLS LAST)
+;
+
 -- Import data using 'copy' command
 -- copy product from '/Users/bishalgautam/Desktop/2021/Hack Reactor/SDC/product.csv' DELIMITER ',' CSV HEADER;
 -- copy characteristics (id, product_id, name) from '/Users/bishalgautam/Desktop/2021/Hack Reactor/SDC/characteristics.csv' DELIMITER ',' CSV HEADER;
