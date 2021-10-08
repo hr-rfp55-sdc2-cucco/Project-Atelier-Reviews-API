fd713d2cd54b7d1f7f50d3ea91188866d47e33ae
Daniel Ho <dho1994@gmail.com>
2021-10-08 12:27:42 -0700

    Modified k6 getReviews test

--- k6/scripts/getReviews.js
+++ k6/scripts/getReviews.js
@@ -1,16 +1,34 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
 
 // Low RPS
+// Use duration 60s to ensure data is sent to New Relic
+// Use duration over iterations b/c can't specify time with iterations; we want 60s
 export const options = {
-  vus: 60,
+  vus: 10,
   duration: '60s',
 };
 
-// High RPS
+// // Mid RPS
 // export const options = {
-//   vus: 1000,
-//   duration: '3s',
+//   stages: [
+//     { duration: '10s', target: 20 }, // below normal load
+//     { duration: '20s', target: 40 }, // normal load
+//     { duration: '10s', target: 60 }, // around the breaking point
+//     { duration: '10s', target: 80 }, // beyond the breaking point
+//     { duration: '10s', target: 0 }, // taper down
+//   ],
+// };
+
+// // High RPS
+// export const options = {
+//   stages: [
+//     { duration: '10s', target: 100 }, // below normal load
+//     { duration: '20s', target: 200 }, // normal load
+//     { duration: '10s', target: 300 }, // around the breaking point
+//     { duration: '10s', target: 400 }, // beyond the breaking point
+//     { duration: '20s', target: 0 }, // taper down
+//   ],
 // };
 
 // Test for a random ID
@@ -22,10 +40,13 @@ export const options = {
 // }
 
 // Test for a random ID in the last 10% of table
-export default function testIdLast10Perc() {
+export default function testIdLast10PercSlow() {
   const maxId = 1000011;
   const idLast10Perc = Math.floor(Math.random() * (maxId * 0.1)) + (Math.floor(maxId * 0.9) + 1);
-  http.get(`http://localhost:8080/reviews?product_id=${idLast10Perc}`);
+  const res = http.get(`http://localhost:8080/reviews?product_id=${idLast10Perc}`);
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
   sleep(1);
 }
 
